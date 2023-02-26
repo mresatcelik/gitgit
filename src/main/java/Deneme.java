a9a714d7efa46f8984cc10ef469a4ad4a641c411
M. Resat Celik <reshat.mrc@gmail.com>
2023-02-26 23:34:07 +0300

    v4

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,5 +2,6 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba Git-GitHup");
         System.out.println("githup a hos geldin");
+        System.out.println("git hupta ne yapacaksın");
     }
 }
